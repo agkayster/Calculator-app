88f28bc88807e33101656c73dad8fe66f211c603
agkayster <ejike.chiboka@gmail.com>
2021-05-26 18:55:33 +0100

    changed to hooks

--- src/CalculatorResultComponent.js
+++ src/CalculatorResultComponent.js
@@ -1,13 +1,23 @@
-import React, { Component } from "react";
-class CalculatorResultComponent extends Component {
-  render() {
-    const { result } = this.props;
-    return (
-      <div className="result">
-        <p>{result}</p>
-      </div>
-    );
-  }
-}
+import React from 'react';
+
+const CalculatorResultComponent = (props) => {
+	const { result } = props;
+	return (
+		<div className='result'>
+			<p>{result}</p>
+		</div>
+	);
+};
 
 export default CalculatorResultComponent;
+
+// class CalculatorResultComponent extends Component {
+// 	render() {
+// 		const { result } = this.props;
+// 		return (
+// 			<div className='result'>
+// 				<p>{result}</p>
+// 			</div>
+// 		);
+// 	}
+// }
